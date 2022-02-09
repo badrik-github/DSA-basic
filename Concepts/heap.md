a74e250fdbf75545afb594cae11059bbece54c1b
PATEL BADRIK MAHESH <patelbadrik@gmail.com>
2022-02-09 09:14:00 +0530

    added heap algo

---
+++ Concepts/heap.md
@@ -0,0 +1,48 @@
+<h1 align="center">
+  	<span>Heap</span>
+</h1>
+
+---
+
+## Definition
+
+- Max-Heap: In a Max-Heap the key present at the root node must be greatest among the keys present at all of it’s children. The same property   must be recursively true for all sub-trees in that Binary Tree.
+
+- Min-Heap: In a Min-Heap the key present at the root node must be minimum among the keys present at all of it’s children. The same property must be recursively true for all sub-trees in that Binary Tree.
+
+
+## Traversing Details
+
+- Parent Node :- (i-1) / 2;
+
+- Current Index :- i;
+
+- Left Child :- (2*i) + 1;
+
+- Right Child :- (2*i) + 2;
+
+## For a array of length N and it is 0 based:-
+
+- Internal Nodes are from 0 to (N/2) - 1;
+
+- Leaf Nodes are from (N/2) to (N-1);
+
+## Time Complexity
+
+# Building a Min_heap or Max_heap takes a time complexity of O(n*log(n))
+- As heapify takes log(n) and to create min or max heap it is called n times so it is O(n*log(n))
+
+# Time complexity of deleting element in heap
+- the time complexity of deleting an element in heap is O(log(n))
+
+# Time complexity of inserting element in heap
+- the time complexity of inserting an element in heap is O(log(n))
+
+# Note:- 
+- Heaps are also used as priority queue.
+
+- Heapify algorithm are also of two type top-down heapify and bottom-up heapify
+
+- top-down heapify is used to delete element
+
+- bottom-up heapify is used to insert element
---
+++ Heap/min_heap.cpp
@@ -0,0 +1,125 @@
+/**
+ * Project - DSA
+ * Mission DSA
+ * @author badrik-github
+ * @since 0.1
+ * @version 0.1
+ */
+
+/*
+
+Problem
+Heapify the array into  min_heap
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int t;
+    t = *x;
+    *x = *y;
+    *y = t;
+}
+
+void Heapify(vector<int> &arr, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (arr[l] < arr[largest] && l < arr.size())
+    {
+        largest = l;
+    }
+
+    if (arr[r] < arr[largest] && r < arr.size())
+    {
+        largest = r;
+    }
+
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        Heapify(arr, largest);
+    }
+}
+
+void deleteElement(vector<int> &arr)
+{
+    swap(arr[0], arr[arr.size() - 1]);
+
+    arr.pop_back();
+
+    Heapify(arr, 0);
+}
+
+void BottomUpHeapify(vector<int> &arr, int index)
+{
+    int parent = (index - 1) / 2;
+
+    if (arr[parent] > arr[index] && parent >= 0)
+    {
+        swap(&arr[parent], &arr[index]);
+        BottomUpHeapify(arr, parent);
+    }
+
+    return;
+}
+
+int main()
+{
+
+    int arrayLength;
+    cout << "Enter the length of array:- ";
+    cin >> arrayLength;
+
+    vector<int> arr;
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+        int temp;
+        cout << "Element " << i << " :- ";
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    cout << endl;
+
+    // creating max_heap
+    for (int i = arr.size() / 2 - 1; i >= 0; i--)
+    {
+        Heapify(arr, i);
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << "->";
+    }
+    cout << endl;
+
+    deleteElement(arr);
+
+    cout << "after deleting" << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << "->";
+    }
+
+    int newElement;
+    cout << endl;
+    cout << "Enter the element you want to insert";
+    cin >> newElement;
+
+    arr.push_back(newElement);
+
+    BottomUpHeapify(arr, arr.size() - 1);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << "->";
+    }
+
+    return 0;
+}
